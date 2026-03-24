b2ffb44ecc91d95a7557c8de08a00cfc4782b8c1
Iury Rodrigues de Godoy <iuryrodriguesgodoy18@gmail.com>
2026-03-24 17:03:41 -0300

    Projeto recaptcha parte 3 - configurando o cors de acesso

--- api/validate-captcha.js
+++ api/validate-captcha.js
@@ -1,5 +1,15 @@
 export default async function handler(req, res) {
 
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "https://icdigitalexperience.com.br");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // resposta para preflight (necessário para fetch do navegador)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       success: false,
