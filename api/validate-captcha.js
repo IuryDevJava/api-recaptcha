10987b0515704b03c43b75e092ab8114c77b55a8
Iury Rodrigues de Godoy <iuryrodriguesgodoy18@gmail.com>
2026-03-24 21:53:30 -0300

    Projeto recaptcha parte 5 - consertando o cors de acesso

--- api/validate-captcha.js
+++ api/validate-captcha.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
 
   // CORS
-  res.setHeader("Access-Control-Allow-Origin", "https://www.icdigitalexperience.com.br/");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.icdigitalexperience.com.br");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
